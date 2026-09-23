f6e606d103cb6444150d83d3d86ec47fa190031e
msojocs <jiyecafe@gmail.com>
2026-09-23 09:28:34 +0800

    fix(ci): unit test

--- backend/internal/server/api_contract_test.go
+++ backend/internal/server/api_contract_test.go
@@ -715,6 +715,7 @@ func TestAPIContracts(t *testing.T) {
 				"message": "success",
 				"data": {
 					"registration_enabled": true,
+					"password_registration_enabled": true,
 					"email_verify_enabled": false,
 					"registration_email_suffix_whitelist": [],
 					"registration_email_domain_quota_enabled": false,
@@ -1066,6 +1067,7 @@ func TestAPIContracts(t *testing.T) {
 				"message": "success",
 				"data": {
 					"registration_enabled": true,
+					"password_registration_enabled": true,
 					"email_verify_enabled": false,
 					"registration_email_suffix_whitelist": [],
 					"registration_email_domain_quota_enabled": false,
